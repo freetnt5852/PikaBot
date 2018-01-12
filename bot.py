9fde23871edd3de4800f39c5157aab9638b35911
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-01-12 21:42:41 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+import sys
+
+bot = commands.Bot(command_prefix="+", description="This is an example bot", owner_id=279974491071709194)
+
+@bot.event
+async def on_ready():
+    print("Bot is online!")
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {bot.ws.latency:.4f * 1000} ms")
+    
+@bot.command()
+async def say(ctx, *, msg: str):
+    await message.delete()
+    await ctx.send(msg)
+    
+bot.run(os.environ.get("TOKEN"))
